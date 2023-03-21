e7ab36b95c96c42fe3b745df3a6d5cef2bbda63b
Vishwas Tamta <vishwastamta@gmail.com>
2023-03-21 17:21:31 +0530

    Prev

--- session 2/index.js
+++ session 2/index.js
@@ -1,28 +1,28 @@
 function clickFn(event) {
-    // const promise = new Promise((resolve, reject) => {
-    //     setTimeout(() => {
-    //         resolve('3 seconds have passed')
-    //     }, 3000)
-    // }); // A promise can be fullfilled/pending/rejected;
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('3 seconds have passed')
+        }, 3000)
+    }); // A promise can haave state of fullfilled/pending/rejected;
 
-    // promise.then((val) => console.log(val))
-    //     .catch(err => console.log(err, 'err'))
-    // console.log('clicked')
-    //insted of logging we will call a fetch API to www.google.com
-    // fetch('http://localhost:8080/creator/login', {
-    //     method: 'POST',
-    //     body: {
-    //         email: 'email.com',
-    //         password: 'password'
-    //     }
-    // })
-    //     .then(() => console.log('Google page oppened!'))
-    //     .catch((err) => console.log(err));
+    promise.then((val) => console.log(val))
+        .catch(err => console.log(err, 'err'))
+    console.log('clicked')
+    // insted of logging we will call a fetch API to www.google.com
+    fetch('http://localhost:8080/creator/login', {
+        method: 'POST',
+        body: {
+            email: 'email.com',
+            password: 'password'
+        }
+    })
+        .then(() => console.log('Google page oppened!'))
+        .catch((err) => console.log(err));
 
     //login form => upon clicking it => function => fetchAPI req send 
     // => to http://localhost:8080/creator/login
     // => attach login email and password field => return 404 not found; fine
     // nav bar in HTML mobile friendly using css;
-    // push your code to github on a repo named sesson 2;
+    // push your code to github on a repo named internship;
     // 11:05 - 4:30pm
 }
